9e9a32a22cc4a04d729080cf1fb577eff272f63d
snowman6-git <chldlfgks55@gmail.com>
2025-02-07 07:42:31 +0900

    into_player 기초 메세지 클래스 관련 표현 수정요구 잘자

--- commands/btn.js
+++ commands/btn.js
@@ -1,5 +1,15 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
+async function into_player(username, uid) {
+  player = {
+    name: username,
+    uid: uid,
+    hp: 50,
+    mental: 0,
+  }
+  return player
+}
+
 module.exports = {
   name: '모집',
   description: '미니게임 모집 테스트',
@@ -24,25 +34,28 @@ module.exports = {
       components: [row],
     });
     const collector = sentMessage.createMessageComponentCollector({
-      time: 60_000,
+      time: 5_000,
     });
 
     let party = []
     collector.on('collect', async buttonInteraction => {
       let username = buttonInteraction.user.globalName
+      let user_id = buttonInteraction.user.id
       if (buttonInteraction.customId === 'next') {
 
         if (!party.includes(username)) {
-          party.push(username)
+          party.push(await into_player(username, user_id))
         } else{
           await interaction.followUp({ 
             content: `-# 이 메세지는 저희가 지워드릴 수 없어요...\n${username}님은 이미 참가하셨어요.`, 
             ephemeral: true, // 해당 유저에게만 보이는 메시지
           })
         }
-
+        
+        //아니면 겜시작하고 플레이어로 만들던가
+        const sorted_party = party.map(item => item.name).toString().replace(",", "\n");
         await buttonInteraction.update({
-          content: `${party.toString().replace(",", "\n")}`,
+          content: `${sorted_party}`,
           components: [row] // 버튼 유지
         });
       } else if (buttonInteraction.customId === 'exit') {
@@ -54,6 +67,18 @@ module.exports = {
         content: '게임을 시작합니다!',
         components: [] // 버튼 제거
       });
+
+      let i = true
+      while(i){
+        party.forEach(turn_of);{
+          await sentMessage.edit({
+            content: `${turn_of.name}의 차례!`,
+            components: [] // 버튼 제거
+          }); 
+        }
+        i = false
+      }
+
       if (collected.size < 2) {
         await sentMessage.edit({
           content: '모집된 인원이 부족합니다! 종료합니다',
